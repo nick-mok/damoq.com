8bc78870f7e004129ac7abab72ac2f20d69be4de
Nicholas <nickmokisasian@gmail.com>
2020-04-05 18:41:24 +1000

    Made header more mobile responsive, still requires more work

--- components/Header.js
+++ components/Header.js
@@ -3,10 +3,10 @@ import Link from 'next/link';
 export const Header = (props) => (
     <div id="header" className="container">
         <div className="row py-2">
-            <div className="col-lg-4 align-self-center">
+            <div className="col-4 align-self-center">
                 <div className=" font-weight-bold text-primary lead">damoq.com</div>
             </div>
-            <div className="col-lg-8 align-self-center">
+            <div className="col-8 align-self-center">
                 <ul className="navbar justify-content-end p-0 m-0">
                     <li className="nav-link mx-1 p-0">
                         <Link href="/">
