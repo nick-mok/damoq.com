efe57cff0cbf614922126ed60fa5b68491e1bf88
Nicholas <nickmokisasian@gmail.com>
2020-04-12 11:49:07 +1000

    Added event to regex replace our slug in state. Matches not words as a group globally

--- pages/slug.js
+++ pages/slug.js
@@ -6,21 +6,23 @@ export const Slug = (props) => {
     const [slug, setSlug] = useState("");
 
     const handleChange = (event) => {
+        const value = event.target.value.toLowerCase();
+        const pattern = /(\W)+/g;
         
-        
-        setSlug(event.target.value);
+        setSlug(value.replace(pattern, "-"));
 
     }
 
     return (
         <Layout pageTitle="Slugpro">
-            <h1>Slug</h1>
+            <h1>Slug me</h1>
             <div>
                 <input type="text" className="slug-field" onChange={handleChange} placeholder="Text you would like to turn into a slug"/>
             </div>
             <div className="mt-2">
                 <input type="text" className="slug-field" value={slug} disabled/>
             </div>
+            <small>^ Triple click to select all</small>
         </Layout>
     );
 }
