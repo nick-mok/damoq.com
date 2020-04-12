bba5ea8f5bddf5ce5815ae55026cd949d9d4a54b
Nicholas <nickmokisasian@gmail.com>
2020-04-12 12:33:22 +1000

    Create new page for later time, unsure how it will be made or what it will be made in, or if it will be made

---
+++ pages/comms.js
@@ -0,0 +1,12 @@
+import Layout from '../components/MyLayout';
+
+export const Comms = (props) => {
+
+    return (
+        <Layout pageTitle="Commendations">
+            <h1>Comming Soon</h1>
+        </Layout>
+    );
+}
+
+export default Comms;
