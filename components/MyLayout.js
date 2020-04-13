fd5291f15a176116f96443ff2f9a0a729d74e240
Nicholas <nickmokisasian@gmail.com>
2020-04-14 07:51:35 +1000

    Added meta tag for email domain configuration

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -6,6 +6,7 @@ const Layout = (props) => (
     <div>
         <Head>
             <title>{props.pageTitle ? props.pageTitle + " - " : ""}Damoq</title>
+            <meta name="yandex-verification" content="ffd1a1d3d375597c" />
         </Head>
         <Header />
         <Main>
