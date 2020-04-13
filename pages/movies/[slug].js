9893d75f3159fb15177e70cae63f449c81d246ad
Nicholas <nickmokisasian@gmail.com>
2020-04-14 07:23:38 +1000

    Created new page for our fallback testing

---
+++ pages/movies/[slug].js
@@ -0,0 +1,13 @@
+import Layout from '../../components/MyLayout';
+
+export const Movie = (props) => {
+
+    return(
+        <Layout pageTitle="Movie">
+            <h1>Movie Name</h1>
+            <p>The movie description</p>
+        </Layout>
+    );
+}
+
+export default Movie;
