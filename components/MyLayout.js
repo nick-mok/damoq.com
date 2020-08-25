785d70a3a02e3d504b2e1a15cb30fe16e3abe9d4
Nicholas <nickmokisasian@gmail.com>
2020-08-25 15:24:52 +1000

    Added meta description

--- components/MyLayout.js
+++ components/MyLayout.js
@@ -7,6 +7,7 @@ const Layout = (props) => (
         <Head>
             <title>{props.pageTitle ? props.pageTitle + " - " : ""}Damoq</title>
             <meta name="yandex-verification" content="ffd1a1d3d375597c" />
+            <meta name="description" content="Damoq is my personal blog for sharing ideas on things web development, pop culture and food"></meta>
             <script type="text/javascript" src="https://www.bugherd.com/sidebarv2.js?apikey=eh78kk9psjjodbmr2pzzxw" async="true"></script>
         </Head>
         <Header />
